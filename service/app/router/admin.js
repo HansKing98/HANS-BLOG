eb1cc983a51a012b73d414d1fb0597b9e1c2e50b
我昊 <im.hansking@gmail.com>
2020-02-23 00:49:00 +0800

    Add 路由守护

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -1,5 +1,6 @@
 module.exports = app => {
     const { router, controller } = app
-    router.get('/admin/index', controller.admin.main.index)
+    const adminauth = app.middleware.adminauth();
+    router.get('/admin/index', adminauth, controller.admin.main.index)
     router.post('/admin/checkLogin', controller.admin.main.checkLogin);
 }
