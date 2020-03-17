26c6cfb48a8250b445127c296d56f54e1d0a2644
我昊 <im.hansking@gmail.com>
2020-03-17 13:11:01 +0800

    Update Admin

--- admin/src/Pages/Main.js
+++ admin/src/Pages/Main.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from './Login'
-import AdminIndex from './AdminIndex'
 
+import Login from "./Login";
+import AdminIndex from "./AdminIndex";
 function Main() {
-    return (
-        <Router>
-            <Route path="/" exact component={Login} />
-            <Route path="/index/" component={AdminIndex} />
-        </Router>
-    )
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={Login} />
+        <Route path="/index/" component={AdminIndex} />
+      </div>
+    </Router>
+  );
 }
-
-export default Main
+export default Main;
--- admin/src/config/apiUrl.js
+++ admin/src/config/apiUrl.js
@@ -1,12 +1,16 @@
-let ipUrl = 'http://127.0.0.1:7001/admin/'
+let ipUrl = "http://127.0.0.1:7001/admin/";
 
 let servicePath = {
-    checkLogin: ipUrl + 'checkLogin',   //  检查用户名密码是否正确
-    getTypeInfo: ipUrl + 'getTypeInfo', //  获得文章类别信息
-    addArticle: ipUrl + 'addArticle',   // 添加文章
-    updateArticle: ipUrl + 'updateArticle', // 修改文章
-    getArticleList: ipUrl + 'getArticleList',   // 文章列表
-    delArticle: ipUrl + 'delArticle/',  // 删除文章
-    getArticleById: ipUrl + 'getArticleById/',  // 根据ID获得文章详情
-}
+	getTypeInfo: ipUrl + "getTypeInfo", // 获得文章类别信息
+	addArticle: ipUrl + "addArticle", // 添加文章
+	updateArticle: ipUrl + "updateArticle", // 添加文章
+	getArticleList: ipUrl + "getArticleList", // 文章列表
+	delArticle: ipUrl + "delArticle/", // 删除文章
+	getArticleById: ipUrl + "getArticleById/", // 根据ID获得文章详情
+	checkLogin: ipUrl + "checkLogin", //  检查用户名密码是否正确
+	checkOpenId: ipUrl + "checkOpenId", // 检查OPendId是否和服务器一样
+	outLogin: ipUrl + "outLogin", // 退出登录
+	updateIsTop: ipUrl + "updateIsTop" // 修改文章是否置顶
+};
+
 export default servicePath;
